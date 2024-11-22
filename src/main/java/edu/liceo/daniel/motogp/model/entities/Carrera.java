b557f5a96e55825c3b15325cca203fa07040010c
imdaano <danisanchezpsc@gmail.com>
2024-11-22 08:49:06 +0100

    add spring code

--- src/main/java/edu/liceo/daniel/motogp/model/entities/Carrera.java
+++ src/main/java/edu/liceo/daniel/motogp/model/entities/Carrera.java
@@ -26,4 +26,60 @@ public class Carrera {
     @Enumerated(EnumType.STRING)
     // revisar porque no puedo poner en mayusculas el enum y cambiar el sql 
     private Posicion posicion;
+
+    
+    public Carrera() {
+    }
+    public Carrera(Integer id, Integer idPiloto, Integer idCircuito, String temporada, Posicion posicion) {
+        this.id = id;
+        this.idPiloto = idPiloto;
+        this.idCircuito = idCircuito;
+        this.temporada = temporada;
+        this.posicion = posicion;
+    }
+    public Carrera(Integer idPiloto, Integer idCircuito, String temporada, Posicion posicion) {
+        this.idPiloto = idPiloto;
+        this.idCircuito = idCircuito;
+        this.temporada = temporada;
+        this.posicion = posicion;
+    }
+    public Integer getId() {
+        return id;
+    }
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Integer getIdPiloto() {
+        return idPiloto;
+    }
+    public void setIdPiloto(Integer idPiloto) {
+        this.idPiloto = idPiloto;
+    }
+    public Integer getIdCircuito() {
+        return idCircuito;
+    }
+    public void setIdCircuito(Integer idCircuito) {
+        this.idCircuito = idCircuito;
+    }
+    public String getTemporada() {
+        return temporada;
+    }
+    public void setTemporada(String temporada) {
+        this.temporada = temporada;
+    }
+    public Posicion getPosicion() {
+        return posicion;
+    }
+    public void setPosicion(Posicion posicion) {
+        this.posicion = posicion;
+    }
+
+    @Override
+    public String toString() {
+        return "Carrera [id=" + id + ", idPiloto=" + idPiloto + ", idCircuito=" + idCircuito + ", temporada="
+                + temporada + ", posicion=" + posicion + "]";
+    }
+
+
+    
 }
