839b4a2695a78fb2004cb37d986d006adf84dc89
imdaano <danisanchezpsc@gmail.com>
2024-11-25 13:31:25 +0100

    add new code

--- src/main/java/edu/liceo/daniel/motogp/model/services/implementations/PilotoService.java
+++ src/main/java/edu/liceo/daniel/motogp/model/services/implementations/PilotoService.java
@@ -1,5 +1,21 @@
 package edu.liceo.daniel.motogp.model.services.implementations;
 
-public class PilotoService {
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import edu.liceo.daniel.motogp.model.entities.Piloto;
+import edu.liceo.daniel.motogp.model.repositories.IPilotoRepository;
+import edu.liceo.daniel.motogp.model.services.interfaces.IPilotoService;
+@Service
+public class PilotoService implements IPilotoService {
+    @Autowired
+    IPilotoRepository pilotosRepo;
+
+    @Override
+    public List<Piloto> conseguirPilotos() {
+       return pilotosRepo.findAll();
+    }
 
 }
--- src/main/java/edu/liceo/daniel/motogp/model/services/interfaces/IPilotoService.java
+++ src/main/java/edu/liceo/daniel/motogp/model/services/interfaces/IPilotoService.java
@@ -1,5 +1,11 @@
 package edu.liceo.daniel.motogp.model.services.interfaces;
 
+import java.util.List;
+
+import edu.liceo.daniel.motogp.model.entities.Piloto;
+
 public interface IPilotoService {
+
+    public List<Piloto> conseguirPilotos();
 
 }
